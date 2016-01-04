dcca2c76076564d463c64fda8fae71a83ebf9e8f
fu <fjchao@gmail.com>
2016-01-05 00:32:47 +0800

    first

---
+++ src/resource.js
@@ -0,0 +1,10 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    BackGround_png : "res/bk.png",
+    Start_png : "res/start1.png",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
